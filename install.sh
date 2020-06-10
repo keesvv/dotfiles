33ab78bc156ad9ae5f0b19010cc36b4b3c0e1295
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-06-10 10:02:28 +0200

    Add support for multiple Ranger plugins

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Definitions
+ranger_plugins=(
+    "https://github.com/alexanderjeurissen/ranger_devicons"
+)
+
 # Link files
 echo "Linking files..."
 ln -srf .zshrc .Xdefaults .vim .vimrc .tmux.conf ~/
@@ -8,11 +13,20 @@ ln -srf .vscode/* ~/.config/Code/User/
 
 # Download ranger plugins
 echo "Updating and installing Ranger plugins..."
-if [ ! -d ~/.config/ranger/plugins/ranger_devicons  ]; then
-    git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-else
-    (cd ~/.config/ranger/plugins/ranger_devicons && git pull)
-fi
+
+for i in "$ranger_plugins"
+do
+    PLUG_NAME=$(echo "$i" | cut -d/ -f5)
+    PLUG_DIR="$HOME/.config/ranger/plugins/$PLUG_NAME"
+
+    echo "$i -> $PLUG_NAME"
+
+    if [ ! -d "$PLUG_DIR"  ]; then
+        git clone "$i" "$PLUG_DIR"
+    else
+        (cd "$PLUG_DIR" && git pull)
+    fi
+done
 
 echo "Done!"
 
