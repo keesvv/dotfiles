2860466b2f9c8592aa085ff9802e001eb3460b8c
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-06-10 09:48:23 +0200

    Update install script & add Ranger plugin

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+# Link files
+echo "Linking files..."
 ln -srf .zshrc .Xdefaults .vim .vimrc .tmux.conf ~/
-ln -sr .config/* ~/.config/
+ln -srf .config/* ~/.config/
+ln -srf .vscode/* ~/.config/Code/User/
+
+# Download ranger plugins
+echo "Updating and installing Ranger plugins..."
+if [ ! -d ~/.config/ranger/plugins/ranger_devicons  ]; then
+    git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+else
+    (cd ~/.config/ranger/plugins/ranger_devicons && git pull)
+fi
+
+echo "Done!"
 
