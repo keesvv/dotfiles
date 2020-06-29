b9cf83690f48e424a4da6c15719dda18e4975484
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-06-29 18:57:18 +0200

    Made install script interactive

--- install.sh
+++ install.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
+RAW_DEPS=$(sed '1d' deps.csv | sort)
+
+while IFS=";" read PACKAGE_NAME IS_OPTIONAL; do
+    CHECKLIST+=("$PACKAGE_NAME")
+    CHECKLIST+=('')
+
+    if [ "$IS_OPTIONAL" = "X" ]; then
+        CHECKLIST+=(off)
+    else
+        CHECKLIST+=(on)
+    fi
+done < <(echo "$RAW_DEPS")
+
+PACKAGES=$(\
+    dialog \
+        --title "Kees' Dotfiles" \
+        --ok-label "Install" \
+        --checklist 'Please select which packages you wish to install:' 20 60 0 "${CHECKLIST[@]}"\
+        3>&1 1>&2 2>&3 3>&- \
+)
+
+clear
+yay -S $PACKAGES
+
+exit
 
 # Definitions
 ranger_plugins=(
