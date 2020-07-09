4fa16d9933044bdcf36cfc4f1d7eac1776aef713
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-07-09 10:29:40 +0200

    Update backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-TARGET="/media/kees/Linux Backup"
+TARGET="/mnt/backup"
 
 rsync -avzP --delete --delete-excluded --stats \
     --exclude "**/.*" \
     --exclude "**/node_modules" \
     --exclude "Source" \
+    --exclude "Backups" \
+    --exclude "Containers" \
     --exclude "Documents/Virtual Machines" \
     /home/kees "$TARGET"
 
