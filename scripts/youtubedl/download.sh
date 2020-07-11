9d51560ff13640fd81fa79293b9dfb1b6fcf5628
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-07-11 10:08:29 +0200

    Moved my youtubedl script to dotfiles

---
+++ scripts/youtubedl/download.sh
@@ -0,0 +1,10 @@
+node extractBookmarks.js
+
+youtube-dl \
+    --no-playlist \
+    --ignore-errors \
+    --batch-file '/tmp/youtubedl-batch' \
+    --no-call-home \
+    --extract-audio \
+    --audio-format mp3 \
+    --output 'output/%(title)s.%(ext)s'
