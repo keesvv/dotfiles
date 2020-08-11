4cc454fb7d3dc1d95435444939c0898a4a3a029f
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-08-11 13:52:45 +0200

    Reorganised RGB scripts & added 'off' script

---
+++ scripts/rgb/init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+FAN_SPEED=35
+
+sudo liquidctl initialize all
+
+sudo liquidctl --match "smart" set fan2 speed $FAN_SPEED
+sudo liquidctl --match "smart" set fan3 speed $FAN_SPEED
--- scripts/rgb/on.sh
+++ scripts/rgb/on.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 MAIN_COLOR=b734eb
-FAN_SPEED=35
-
-sudo liquidctl initialize all
 
 sudo liquidctl --match "smart" set led1 color fixed $MAIN_COLOR
 sudo liquidctl --match "smart" set led2 color fixed $MAIN_COLOR
-
-sudo liquidctl --match "smart" set fan2 speed $FAN_SPEED
-sudo liquidctl --match "smart" set fan3 speed $FAN_SPEED
-
 sudo liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed fastest
+sudo liquidctl --match "kraken" set logo color fixed ffffff
