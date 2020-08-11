5907d7a8d2c586c52a7868383bacfb192167e0cf
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-08-11 11:56:00 +0200

    Update fan speed

--- scripts/rgb.sh
+++ scripts/rgb.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 MAIN_COLOR=b734eb
+FAN_SPEED=35
 
 sudo liquidctl initialize all
 
 sudo liquidctl --match "smart" set led1 color fixed $MAIN_COLOR
 sudo liquidctl --match "smart" set led2 color fixed $MAIN_COLOR
 
-sudo liquidctl --match "smart" set fan2 speed 40
-sudo liquidctl --match "smart" set fan3 speed 40
+sudo liquidctl --match "smart" set fan2 speed $FAN_SPEED
+sudo liquidctl --match "smart" set fan3 speed $FAN_SPEED
 
 sudo liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed fastest
