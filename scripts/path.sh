de93cbf0636daf83b3c34674a7f28496367baa8e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-09-26 18:35:29 +0200

    Update PATH

--- scripts/path.sh
+++ scripts/path.sh
@@ -4,6 +4,7 @@ PATH="$PATH:/home/kees/.local/bin"
 PATH="$PATH:/home/kees/.nvm/versions/node/v12.16.1/bin"
 PATH="$PATH:/home/kees/.yarn/bin"
 PATH="$PATH:/home/kees/Source/flutter/bin"
+PATH="$PATH:/home/kees/.cargo/bin"
 
 export PATH
 
