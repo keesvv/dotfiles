ecf799215d88810ecaaa8856513136c14ae7cd93
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-11-13 11:00:41 +0100

    Fix tap-to-click not working

--- scripts/init/xorg.sh
+++ scripts/init/xorg.sh
@@ -6,8 +6,8 @@ xinput --set-prop $MOUSE_DEVID 'libinput Accel Speed' $MOUSE_ACCEL
 # Touchpad options (if available)
 if [ ! -z "$TOUCHPAD_DEVID"  ]; then
     xinput --set-prop $TOUCHPAD_DEVID 'libinput Accel Speed' $TOUCHPAD_ACCEL
-    xinput --set-prop $TOUCHPAD_DEVID 'libinput Tapping Enabled' 1
     xinput --set-prop $TOUCHPAD_DEVID 'libinput Natural Scrolling Enabled' 1
+    xinput --set-prop $TOUCHPAD_DEVID 'Synaptics Tap Action' 1, 1, 1, 1, 1, 1, 1
 fi
 
 # Keyboard options 
