5820d5a4abb5509267531f72bca46732d541eb1d
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-11-13 10:17:45 +0100

    Switch back to bspwm

--- scripts/init/exec.sh
+++ scripts/init/exec.sh
@@ -10,7 +10,7 @@ function run_once {
 # Run background tasks
 killall -q sxhkd; sxhkd &
 killall -q picom; sleep 0.3s; picom &
-#killall -q polybar; polybar mainbar-bspwm &
+killall -q polybar; polybar mainbar-bspwm &
 
 run_once redshift
 run_once nextcloud
