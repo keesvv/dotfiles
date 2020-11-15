675ef8ca8624f5138b0bcd5146298f7f4a4d7dce
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-11-15 20:15:44 +0100

    Fix incorrect touchpad tap action order

--- scripts/init/xorg.sh
+++ scripts/init/xorg.sh
@@ -7,7 +7,7 @@ xinput --set-prop $MOUSE_DEVID 'libinput Accel Speed' $MOUSE_ACCEL
 if [ ! -z "$TOUCHPAD_DEVID"  ]; then
     xinput --set-prop $TOUCHPAD_DEVID 'Device Accel Constant Deceleration' $TOUCHPAD_ACCEL
     xinput --set-prop $TOUCHPAD_DEVID 'Device Accel Adaptive Deceleration' 1
-    xinput --set-prop $TOUCHPAD_DEVID 'Synaptics Tap Action' 1, 1, 1, 1, 1, 1, 1
+    xinput --set-prop $TOUCHPAD_DEVID 'Synaptics Tap Action' 0, 0, 0, 0, 1, 3, 2
 fi
 
 # Keyboard options 
