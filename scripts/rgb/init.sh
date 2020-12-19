f54560d12e7b706d25c17ac76e51d65c90b6006d
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-12-19 10:04:39 +0100

    Execute liquidctl as regular user

--- scripts/rgb/init.sh
+++ scripts/rgb/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 FAN_SPEED=35
 
-sudo liquidctl initialize all
+liquidctl initialize all
 
-sudo liquidctl --match "smart" set fan2 speed $FAN_SPEED
-sudo liquidctl --match "smart" set fan3 speed $FAN_SPEED
+liquidctl --match "smart" set fan2 speed $FAN_SPEED
+liquidctl --match "smart" set fan3 speed $FAN_SPEED
 
 source ~/.dotfiles/scripts/rgb/on.sh
--- scripts/rgb/off.sh
+++ scripts/rgb/off.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo liquidctl --match "smart" set led1 color fixed 000000 
-sudo liquidctl --match "smart" set led2 color fixed 000000
-sudo liquidctl --match "kraken" set ring color fixed 000000
-sudo liquidctl --match "kraken" set logo color fixed 000000
+liquidctl --match "smart" set led1 color fixed 000000 
+liquidctl --match "smart" set led2 color fixed 000000
+liquidctl --match "kraken" set ring color fixed 000000
+liquidctl --match "kraken" set logo color fixed 000000
