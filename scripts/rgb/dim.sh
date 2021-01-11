56e3709322a515b499b261ee46b7c898afe0c2a2
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-11 17:07:41 +0100

    Dimmed MAIN_COLOR even more

--- scripts/rgb/dim.sh
+++ scripts/rgb/dim.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-MAIN_COLOR=69098f
+MAIN_COLOR=250333
 
-liquidctl --match "smart" set led1 color fixed $MAIN_COLOR
-liquidctl --match "smart" set led2 color fixed $MAIN_COLOR
-liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed slowest
-liquidctl --match "kraken" set logo color fixed ffffff
+liquidctl --match "smart" set led1 color fixed $MAIN_COLOR &
+liquidctl --match "smart" set led2 color fixed $MAIN_COLOR &
+liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed slowest &
+liquidctl --match "kraken" set logo color fixed 8c8c8c &
 
-python3 ~/.dotfiles/scripts/hue/main.py chill
+python3 ~/.dotfiles/scripts/hue/main.py chill &
