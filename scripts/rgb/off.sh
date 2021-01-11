1220263c2cdb2dfd93e00428e6827e043d0074a7
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-11 17:00:09 +0100

    Speed up scripts by queuing as jobs

--- scripts/rgb/off.sh
+++ scripts/rgb/off.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-liquidctl --match "smart" set led1 color fixed 000000 
-liquidctl --match "smart" set led2 color fixed 000000
-liquidctl --match "kraken" set ring color fixed 000000
-liquidctl --match "kraken" set logo color fixed 000000
+liquidctl --match "smart" set led1 color fixed 000000 &
+liquidctl --match "smart" set led2 color fixed 000000 &
+liquidctl --match "kraken" set ring color fixed 000000 &
+liquidctl --match "kraken" set logo color fixed 000000 &
 
-python3 ~/.dotfiles/scripts/hue/main.py off
+python3 ~/.dotfiles/scripts/hue/main.py off &
--- scripts/rgb/on.sh
+++ scripts/rgb/on.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 MAIN_COLOR=b734eb
 
-liquidctl --match "smart" set led1 color fixed $MAIN_COLOR
-liquidctl --match "smart" set led2 color fixed $MAIN_COLOR
-liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed fastest
-liquidctl --match "kraken" set logo color fixed ffffff
+liquidctl --match "smart" set led1 color fixed $MAIN_COLOR &
+liquidctl --match "smart" set led2 color fixed $MAIN_COLOR &
+liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed fastest &
+liquidctl --match "kraken" set logo color fixed ffffff &
 
-python3 ~/.dotfiles/scripts/hue/main.py focus
+python3 ~/.dotfiles/scripts/hue/main.py focus &
