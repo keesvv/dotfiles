c4017c1b50ff4f04450f0bf9d63b57448f2272c5
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-11 11:20:59 +0100

    Adjust Hue lights when enabling RGB

--- scripts/rgb/on.sh
+++ scripts/rgb/on.sh
@@ -5,3 +5,5 @@ sudo liquidctl --match "smart" set led1 color fixed $MAIN_COLOR
 sudo liquidctl --match "smart" set led2 color fixed $MAIN_COLOR
 sudo liquidctl --match "kraken" set ring color tai-chi $MAIN_COLOR --speed fastest
 sudo liquidctl --match "kraken" set logo color fixed ffffff
+
+python3 ~/.dotfiles/scripts/hue/main.py focus
