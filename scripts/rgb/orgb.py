481061155a1486ac3dca77331a908c34690a093d
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-11 18:20:20 +0100

    Add support for dim mode

--- scripts/rgb/orgb.py
+++ scripts/rgb/orgb.py
@@ -1,11 +1,15 @@
 import sys
 from openrgb import OpenRGBClient
 from openrgb.orgb import Device
-from openrgb.utils import DeviceType
+from openrgb.utils import DeviceType, RGBColor
 
 def setMode(devices: list[Device], mode: str):
     for device in devices:
         device.set_mode(mode)
+
+def setColor(devices: list[Device], color: RGBColor):
+    for device in devices:
+        device.set_color(color)
 
 if len(sys.argv) < 2:
     sys.exit(1)
@@ -20,3 +24,9 @@ if sys.argv[1] == "off":
 elif sys.argv[1] == "on":
     setMode(dram, "rainbow")
     setMode(mobo_led, "rainbow")
+elif sys.argv[1] == "dim":
+    color = RGBColor(75, 10, 140)
+    setMode(dram, "breathing")
+    setMode(mobo_led, "direct")
+    setColor(dram, color)
+    setColor(mobo_led, color)
