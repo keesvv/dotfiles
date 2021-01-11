99284a3bcb49510a883299b64f767b1ae993d464
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-01-11 18:25:16 +0100

    Reorder RGB init script

--- scripts/init/init.sh
+++ scripts/init/init.sh
@@ -3,11 +3,8 @@
 # Set wallpaper
 feh --bg-scale ~/.dotfiles/.wallpaper.jpg
 
-# Control lights on login
-~/.dotfiles/scripts/autohue/login.sh
-
-# Set up NZXT RGB control
-~/.dotfiles/scripts/rgb/init.sh
-
 source ~/.dotfiles/scripts/init/xorg.sh
 source ~/.dotfiles/scripts/init/exec.sh
+
+# Set up RGB
+~/.dotfiles/scripts/rgb/init.sh
