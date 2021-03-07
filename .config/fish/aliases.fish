03c4fcfdd9e6975e7edf91940bdc378343db5f6e
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2021-03-07 10:42:13 +0100

    Fix hardcoded `vi` in aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -34,8 +34,8 @@ alias open='xdg-open'
 alias btconnect='bluetoothctl power on && sleep 1 && bluetoothctl connect 7C:38:AD:24:09:C5'
 
 # Edit bspwm config
-alias bspe='vi ~/.dotfiles/.config/bspwm/bspwmrc'
-alias keyedit='vi ~/.dotfiles/.config/sxhkd/sxhkdrc'
+alias bspe='$EDITOR ~/.dotfiles/.config/bspwm/bspwmrc'
+alias keyedit='$EDITOR ~/.dotfiles/.config/sxhkd/sxhkdrc'
 
 # Editors
 alias m='micro'
