be839fd6442e2cfd993c0dc0977808ceb33b0427
Kees van Voorthuizen <me@keesvv.nl>
2021-03-08 22:48:50 +0100

    Update $PATH

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -5,6 +5,6 @@ set PATH "$PATH:/home/kees/.yarn/bin"
 set PATH "$PATH:/home/kees/Source/flutter/bin"
 set PATH "$PATH:/home/kees/.cargo/bin"
 set PATH "$PATH:/home/kees/.gem/ruby/2.7.0/bin"
-set PATH "$PATH:/home/kees/go/bin"
+set PATH "$PATH:$GOPATH/bin"
 
 export PATH
