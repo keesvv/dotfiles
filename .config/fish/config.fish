097d9d793387123b733e09de328f94d883ef9824
Kees van Voorthuizen <me@keesvv.nl>
2021-04-03 22:49:30 +0200

    Add Lua paths to Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ export SSH_KEY_PATH="~/.ssh/id_rsa"
 export GPG_TTY=(tty)
 export FZF_DEFAULT_COMMAND="fd -t file"
 export HSTR_CONFIG=hicolor
+export LUA_CPATH='./?.so;/usr/lib64/lua/5.1/?.so;/usr/lib64/lua/5.1/loadall.so;/home/kees/.luarocks/lib/lua/5.1/?.so;/usr/share/lua/5.1/lib/lua/5.1/?.so'
+export LUA_PATH='/usr/share/lua/5.1/?.lua;./?.lua;/usr/share/lua/5.1/?/init.lua;/usr/lib64/lua/5.1/?.lua;/usr/lib64/lua/5.1/?/init.lua;/home/kees/.luarocks/share/lua/5.1/?.lua;/home/kees/.luarocks/share/lua/5.1/?/init.lua;/usr/share/lua/5.1/share/lua/5.1/?.lua;/usr/share/lua/5.1/share/lua/5.1/?/init.lua'
 
 # Go
 set -gx GOPATH "$HOME/.go"
